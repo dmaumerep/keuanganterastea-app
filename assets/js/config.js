6592625f33954de8567725728512b4dd4707b5d3
dmaumerep <maumere.putra@gmail.com>
2026-07-01 09:18:55 +0700

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,1 +1,8 @@
-const CONFIG = {};
+const CONFIG = {
+
+    APP_NAME: "Sistem Keuangan Teras Tea",
+
+    API_URL:
+        "https://script.google.com/macros/s/AKfycbwxyapiD0XDAFBRRcichgjI3DKNkSrw1aiOTS6_xUPyNtxyX5tJaOepQbSZ3qLqNRgDQg/exec"
+
+};
