ebbf16bab8b3f91ccf90d6e318b2dca483f32aa1
dmaumerep <maumere.putra@gmail.com>
2026-07-01 09:20:33 +0700

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,1 +1,77 @@
 // API Helper
+
+async function apiGet(action) {
+
+    try {
+
+        const response = await fetch(
+            `${CONFIG.API_URL}?action=${action}`
+        );
+
+        return await response.json();
+
+    }
+
+    catch (error) {
+
+        console.error(error);
+
+        return {
+
+            success: false,
+
+            message:
+                "Gagal terhubung ke server"
+
+        };
+
+    }
+
+}
+
+async function apiPost(data) {
+
+    try {
+
+        const response = await fetch(
+
+            CONFIG.API_URL,
+
+            {
+
+                method: "POST",
+
+                headers: {
+
+                    "Content-Type":
+                        "application/json"
+
+                },
+
+                body:
+                    JSON.stringify(data)
+
+            }
+
+        );
+
+        return await response.json();
+
+    }
+
+    catch (error) {
+
+        console.error(error);
+
+        return {
+
+            success: false,
+
+            message:
+                "Gagal terhubung ke server"
+
+        };
+
+    }
+
+}
